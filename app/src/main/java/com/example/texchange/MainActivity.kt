ca3680ec0e2f91100393f10ac6da029586fc6fc6
khantact <kevinhan0@gmail.com>
2024-03-18 12:16:02 -0400

    Sign up page

--- app/src/main/java/com/example/texchange/MainActivity.kt
+++ app/src/main/java/com/example/texchange/MainActivity.kt
@@ -11,11 +11,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.texchange.ui.theme.TexChangeTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+    public override fun onStart(){
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            reload()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         setContentView(R.layout.logo_page)
+    }
+
+    private fun reload() {
     }
 }
 
---
+++ app/src/main/java/com/example/texchange/RegisterActivity.kt
@@ -0,0 +1,5 @@
+package com.example.texchange
+
+class RegisterActivity {
+
+}
